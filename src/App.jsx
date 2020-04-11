4e5fa29c062bf7a244f2af52926c12e9b6ea9029
Joseph Yusufov <jyusufov00@stuy.edu>
2020-04-11 13:02:06 -0400

    started some shvalid work

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Clock from './components/Clock.jsx';
+
+function App(){
+    return(
+        <div>
+            <h1>Timone Eskaros, this is my app</h1>
+            <p>This is the timone app.</p>
+            <Clock/>
+        </div>
+    );
+};
+
+export default App;
